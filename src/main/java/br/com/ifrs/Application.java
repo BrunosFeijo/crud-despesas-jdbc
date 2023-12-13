16e8dbbb2327a506ae3831be26c605b3071e9ec5
BrunosFeijo <bruno.s.feijo@hotmail.com>
2023-12-13 00:59:35 -0300

    findAll

--- src/main/java/br/com/ifrs/Application.java
+++ src/main/java/br/com/ifrs/Application.java
@@ -5,18 +5,31 @@ import br.com.ifrs.model.Categoria;
 import br.com.ifrs.model.Despesa;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         DespesaDAO dao = new DespesaDAO();
+        List<Despesa> despesas = dao.findAll();
 
-        Despesa despesa = new Despesa("Livro de Prog",
-                LocalDate.of(2023,12,9),
-                150,
-                Categoria.EDUCACAO);
+        for (Despesa despesa : despesas) {
+            System.out.println("Id: " + despesa.getId());
+            System.out.println("Descrição: " + despesa.getDescricao());
+            System.out.println("Data: " + despesa.getData());
+            System.out.println("Valor: " + despesa.getValor());
+            System.out.println("Categoria: " + despesa.getCategoria());
+            System.out.println();
+        }
 
-        Despesa despesaInsercao = dao.save(despesa);
-        System.out.println("Foi inserido a despesa com id: " + despesaInsercao.getId());
+
+//        Despesa despesa = new Despesa("Viagem de uber até o aeroporto",
+//                LocalDate.of(2023,12,28),
+//                110,
+//                Categoria.TRANSPORTE);
+//
+//        Despesa despesaInsercao = dao.save(despesa);
+//        System.out.println("Foi inserido a despesa com id: " + despesaInsercao.getId());
 
     }
 }
--- src/main/java/br/com/ifrs/dao/DespesaDAO.java
+++ src/main/java/br/com/ifrs/dao/DespesaDAO.java
@@ -5,16 +5,19 @@ import br.com.ifrs.model.Categoria;
 import br.com.ifrs.model.Despesa;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class DespesaDAO implements IDespesasDAO{
+public class DespesaDAO implements IDespesasDAO {
 
     @Override
     public Despesa save(Despesa despesa) {
-        try (Connection connection = ConnectionFactory.getConnection()){
-            String sql = "INSERT INTO Despesas (descricao, valor, data, categoria) VALUES (?,?,?,?)";
-            PreparedStatement preparedStatement =  connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        String sql = "INSERT INTO Despesas (descricao, valor, data, categoria) VALUES (?,?,?,?)";
+
+        try (Connection connection = ConnectionFactory.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatement.setString(1, despesa.getDescricao());
             preparedStatement.setDouble(2, despesa.getValor());
@@ -22,7 +25,7 @@ public class DespesaDAO implements IDespesasDAO{
             preparedStatement.setString(4, despesa.getCategoria().toString());
 
             preparedStatement.executeUpdate();
-            ResultSet resultSet=  preparedStatement.getGeneratedKeys();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
             resultSet.next();
 
             Long id = resultSet.getLong("id");
@@ -49,7 +52,30 @@ public class DespesaDAO implements IDespesasDAO{
 
     @Override
     public List<Despesa> findAll() {
-        return null;
+        String sql = "SELECT id, descricao, data, valor, categoria FROM despesas";
+        List<Despesa> despesas = new ArrayList<>();
+
+        try (Connection connection = ConnectionFactory.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String descricao = resultSet.getString("descricao");
+                Double valor =  resultSet.getDouble("valor");
+                LocalDate data = resultSet.getDate("data").toLocalDate();
+                Categoria categoria = Categoria.valueOf(resultSet.getString("categoria"));
+
+                Despesa despesa = new Despesa(id, descricao, data, valor, categoria);
+                despesas.add(despesa);
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return despesas;
     }
 
     @Override
